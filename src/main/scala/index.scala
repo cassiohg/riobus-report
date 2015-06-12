835427cb359c64a943032c03c88c63e82218c922
Cassiohg <cassiohg@gmail.com>
2015-06-12 01:10:55 -0300

    renamed file and continued code
    code now prints the 10 first registries with speed over 60 km/h

--- src/main/scala/index.scala
+++ src/main/scala/index.scala
@@ -3,22 +3,27 @@ import java.io.{FileReader, FileNotFoundException, IOException}
 
 
 object RiobusReport {
-	def main(args: Array[String]) {
-		val filename = "./resources/abc.txt"
-		// val filename = "/Users/cassiohg/Downloads/riobusData/estudo_cassio_part_000000000000.csv"
+
+	def speedLimit(speed: Int) {
+		// val filename = "./resources/abc.txt"
+		val filename = "/Users/cassiohg/Downloads/riobusData/estudo_cassio_part_000000000000.csv"
 		try {
-			val speeds = scala.io.Source.fromFile(filename)
+			val speedsFiltered = scala.io.Source.fromFile(filename)
 				.getLines
 				.drop(1)
 				.toList
 				.map(x => x.split(",").toList)
-				.filter(x => x(2).nonEmpty)
-				.map(x => x(2).toFloat)
-				// .filter(x => x(2) >= 10)
-			println(speeds.take(10))
+				.filter(x => x(5).nonEmpty)
+				.filter(x => x(5).toFloat > speed)
+				.take(10)
+				.foreach(println)
 		} catch {
 			case ex: FileNotFoundException => println("Couldn't find that file.")
 			case ex: IOException => println("Had an IOException trying to read that file")
 		}
+	}
+
+	def main(args: Array[String]) {
+		speedLimit(60)
 	}
 }
