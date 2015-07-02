a3defdcb138216d9f914b09a4092caa9b27d18a9
Cassiohg <cassiohg@gmail.com>
2015-07-02 11:43:42 -0300

    writing one file instead of two
    the size of the result is now written in the first line of the result file

--- src/main/scala/index.scala
+++ src/main/scala/index.scala
@@ -17,28 +17,26 @@ object RiobusReport {
 		val filename = path + "riobusData/estudo_cassio_part_000000000000.csv" // path to file.
 
 		val filteredSpeeds = sc.textFile(filename, 2).cache() // reading text file with 2 copies, then caching on memory.
-			.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter } // droping first line of the RDD.
+			.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter } //droping first line of the RDD.
 			.map(x => x.split(",").toList) // spliting each line by commas.
 			.filter(x => x(5).nonEmpty) // removing lines with empty values in the 6th column.
 			.filter(x => x(5).toFloat > speed) // filtering lines with value of 6th column, parsed to float, above 'speed'.
 
 		
 
-		// we will need the size of this result.
-		val pw = new PrintWriter(new File(path + "result/speedLimit-resultCount.txt"), "UTF-8")
-		pw.write(filteredSpeeds.count().toString) // printing the amount of lines left.
-		pw.close
+		// we will need to write in a file the argument we have received (as a confirmation), the size of the result and 
+		// a sample of it, of a small size.
+		val pw = new PrintWriter(new File(path + "result/speedLimit-result.txt"), "UTF-8") // creating file.
+		pw.write(speed.toString + "\n") // writing argument we have received.
+		pw.write(filteredSpeeds.count().toString + "\n") // writing the amount of records in the result.
 
-		// we will need some sample of the result, just to take a look at.
-		val pw2 = new PrintWriter(new File(path + "result/speedLimit-result.txt"), "UTF-8")
-		filteredSpeeds.take(10).foreach(x => pw2.write((x mkString ",") + "\n")) // printing the lines converted to strings.
-		pw2.close
-		
+		// each element of 'filteredSpeeds' needs to be concatenated and converted to string before being written in file.
+		filteredSpeeds.take(10).foreach(x => pw.write((x mkString ",") + "\n")) // writing the first 10 records.
+		pw.close // closing file.
 	}
 
 	def main(args: Array[String]) {
-		
-		speedLimit(60)
+		speedLimit(args(0).toInt) // reading speed from arguments
 	}
 }
 
