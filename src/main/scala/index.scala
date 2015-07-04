a42791416fa81efff143c92d492b8dbd81190a95
Cassiohg <cassiohg@gmail.com>
2015-07-04 16:07:27 -0300

    also filtering by date interval and rectangle area

--- src/main/scala/index.scala
+++ src/main/scala/index.scala
@@ -1,39 +1,79 @@
 
 import java.io._
+import java.util.Date
+import java.text.SimpleDateFormat
 
 import org.apache.spark.{SparkConf, SparkContext}
 
-object RiobusReport {
-
-	val conf = new SparkConf().setAppName("RiobusReport").setMaster("spark://JonnyLaptop:7077")
+object  myApp {
+	val conf = new SparkConf().setAppName("RiobusReport")//.setMaster("spark://localhost:7077")
     val sc = new SparkContext(conf)
 
-    // method that will implement the speed limit report. It will filter every register that has speed above given value.
-	def speedLimit(speed: Int) {
-		val path = "/mnt/jonny-data/Documentos/UFRJ/BigData/riobus-data/" // path to project.
+	val path = "/Users/cassiohg/Coding/Scala/riobus-report/" // path to project.
+	val filenameAndPath = path + "riobusData/estudo_cassio_part_000000000000.csv" // path to file being read.
+	val resultFilenameAndPath = path + "result/speedLimit-result.txt" // path to file that will be written.
 
-		val filename = path + "estudo_cassio_part_000000000000.csv" // path to file.
+	val dateFormatGoogle = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss 'UTC'") // format used by the data we have.
+	val dateFormathttp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss") // format we use inside http message.
 
-		val filteredSpeeds = sc.textFile(filename, 2).cache() // reading text file with 2 copies, then caching on memory.
-			.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter } //droping first line of the RDD.
-			.map(x => x.split(",").toList) // spliting each line by commas.
-			.filter(x => x(5).nonEmpty) // removing lines with empty values in the 6th column.
-			.filter(x => x(5).toFloat > speed) // filtering lines with value of 6th column, parsed to float, above 'speed'.
+	def main(args: Array[String]) {
+		val speed = args(0).toDouble // value that will hold the speed we will compare to every register speed value.
+		val dateBeggin = dateFormathttp.parse(args(1)) // value date that will hold the beggining of the date range.
+		val dateEnd = dateFormathttp.parse(args(2)) // value date that will hold the end of the date range.
+		val rectangleX = args(3).toDouble // value date that will hold the x value for the rectangle anchor point.
+		val rectangleY = args(4).toDouble // value date that will hold the y value for the rectangle anchor point.
+		val rectangleLength = args(5).toDouble // value date that will hold rectangle's legnth.
+		val rectangleHeight = args(6).toDouble // value date that will hold rectangle's height.
+		
+		// Defining functions instead of methods. I am doing it because 'myApp' is an object, not a class, so I don't have
+		// a constructor for it. That means I can't use the arguments 'args' out of 'main' method.
 
+		// returns true if 'speedString' is not emmpty and it's value converted to float is bigger than 'speed' (arg(0)).
+		val isAboveSpeed = {(speedString: String) => speedString.nonEmpty && speedString.toFloat > speed}
 
+		// returns true if 'stringDate', converted to Date object is bigger than 'dateBeggin' and smaller than 'dateEnd'.
+		val isdateInsideRange = {(stringDate: String) =>
+			//converting string to a date using pattern inside 'dateFormatGoogle'.
+			var date = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss 'UTC'")).parse(stringDate)
+			// appearenlty I can't use the 'dateFormatGoogle' because 'SimpleDateFormat' is not thread safe. I need a
+			// new instance for every thread, but I don't know how to do it in an spark application. That's why I create a
+			// new instance inside every function call.
+
+			// testing if date is inside date range.
+			date.compareTo(dateBeggin) >= 0 && date.compareTo(dateEnd) <= 0
+		}
+
+		// returns true if latitude and longitude are inside the rectangle defined by the 
+		// arguments given to this application.
+		val isInsideRectangle = {(stringX: String, stringY: String) =>
+			var x = stringX.toDouble // converting string to float.
+			var y = stringY.toDouble // converting string to float.
+			// testing if x and y are inside ractangle boundaries.
+			x >= rectangleX && x <= rectangleX + rectangleLength && y <= rectangleY && y >= rectangleY + rectangleHeight
+		}
+
+		// Code that will implement the speed limit report. It will filter every register that has speed above given value.
+
+		// reading text file with 2 copies, then caching on memory.
+		val filteredSpeeds = sc.textFile(filenameAndPath, 2).cache()
+			//dropping first line of the RDD.
+			.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter } 
+			// spliting each line by commas.
+			.map(x => x.split(",").toList) 
+			// filtering lines with value of 6th column above 'speed' that are not empty, are inside date range
+			// range and rectangle area given in argument.
+			.filter(x => isAboveSpeed(x(5)) && isdateInsideRange(x(0)) && isInsideRectangle(x(3), x(4)) )
 
 		// we will need to write in a file the argument we have received (as a confirmation), the size of the result and 
 		// a sample of it, of a small size.
-		val pw = new PrintWriter(new File(path + "result/speedLimit-result.txt"), "UTF-8") // creating file.
-		pw.write(speed.toString + "\n") // writing argument we have received.
-		pw.write(filteredSpeeds.count().toString + "\n") // writing the amount of records in the result.
+		val pw = new PrintWriter(new File(resultFilenameAndPath), "UTF-8") // creating file to be written on.
+		// writing the arguments we have received. just to give a feedback.
+		pw.write(args(0)+","+args(1)+","+args(2)+","+args(3)+","+args(4)+","+args(5)+","+args(6)+ "\n")
+		// writing the amount of records in the result.
+		pw.write(filteredSpeeds.count().toString + "\n")
 
 		// each element of 'filteredSpeeds' needs to be concatenated and converted to string before being written in file.
-		filteredSpeeds.take(10).foreach(x => pw.write((x mkString ",") + "\n")) // writing the first 10 records.
+		filteredSpeeds.take(200).foreach(x => pw.write((x mkString ",") + "\n")) // writing the first 200 records.
 		pw.close // closing file.
-	}
-
-	def main(args: Array[String]) {
-		speedLimit(args(0).toInt) // reading speed from arguments
 	}
 }
